1432c9af1a113db7ff44044be3fb88ccf744606b
Xander <acox9567@bths.edu>
2018-09-18 10:14:44 -0400

    End of class update.

--- src/aCox/library.java
+++ src/aCox/library.java
@@ -6,4 +6,44 @@ public class library
 	{
 		System.out.println(str);
 	}
+
+	public static boolean isPalendrome(String str)
+	{
+		String rev = "";
+
+		for(int i = str.length() - 1; i >= 0; i--)
+			rev += str.substring(i, i + 1);
+
+		return (rev.equals(str));
+	}
+
+	public static String cutOut(String mainStr, String subStr)
+	{
+		int idx = mainStr.indexOf(subStr);
+
+		if (idx == -1)
+			return mainStr;
+
+		String sub1 = mainStr.substring(0, idx);
+		String sub2 = mainStr.substring(idx + subStr.length());
+
+		return (sub1 + sub2);
+	}
+
+	public static void multiplicationTable(int base, int range)
+	{
+		for (int i = 0; i <= range; i++)
+			System.out.print(String.valueOf(base * i) + ",");
+		System.out.print("\n");
+	}
+
+	public static int sumUpTo(int num)
+	{
+		int val = 0;
+
+		for (int i = 1; i <= num; i ++)
+			val += i;
+
+		return val;
+	}
 }
--- src/aCox/testFile.java
+++ src/aCox/testFile.java
@@ -1,11 +1,19 @@
 package aCox;
 
 import static aCox.library.println;
+import static aCox.library.isPalendrome;
+import static aCox.library.cutOut;
+import static aCox.library.multiplicationTable;
+import static aCox.library.sumUpTo;
 
 public class testFile
 {
     public static void main(String[] args)
     {
         println("I love a cup of java in the morning");
+        println(Boolean.toString(isPalendrome("racecar")));
+        println(cutOut("catatonic cat", "cat"));
+        multiplicationTable(5,8);
+        println(String.valueOf(sumUpTo(4)));
     }
 }

db196f6b2ab2ee875abb5bbafce9b98e4955c5f2
Alexander Cox <acox9567>
2018-09-20 10:19:08 -0400

    End of class.

--- src/aCox/testFile.java
+++ src/aCox/testFile.java
@@ -1,19 +1,58 @@
 package aCox;
 
 import static aCox.library.println;
-import static aCox.library.isPalendrome;
+import static aCox.library.isPalindrome;
 import static aCox.library.cutOut;
 import static aCox.library.multiplicationTable;
 import static aCox.library.sumUpTo;
+import static aCox.library.dateStr;
 
 public class testFile
 {
     public static void main(String[] args)
     {
         println("I love a cup of java in the morning");
-        println(Boolean.toString(isPalendrome("racecar")));
-        println(cutOut("catatonic cat", "cat"));
+
+        println("");
+
+        //Test of isPalindrome method
+        boolean a = isPalindrome("racecar");
+        boolean b = isPalindrome("java");
+        boolean c = isPalindrome("dad");
+        println("true: " + Boolean.toString(a) + "\nfalse: " + Boolean.toString(b) + "\ntrue: " + Boolean.toString(c));
+
+        println("");
+
+        //Test of dateStr method
+        String d = dateStr("01/04/2001");
+        String e = dateStr("04/29/2001");
+        String f = dateStr("12/25/2018");
+        println("01-04-2001: " + d + "\n04-29-2001: " + e + "\n12-25-2018: " + f);
+
+        println("");
+
+        //Test of cutOut method
+        d  = cutOut("catatonic cat", "cat");
+        e  = cutOut("notebook", "book");
+        f  = cutOut("apple", "le");
+        println("atonic cat: " + d  + "\nnote: " + e + "\napp: " + f);
+
+        println("");
+
+        //Test of multiplicationTable method
+        println("0 5 10 15 20 25 30 35 40 ");
         multiplicationTable(5,8);
-        println(String.valueOf(sumUpTo(4)));
+        println("0 2 4 6 8 ");
+        multiplicationTable(2,4);
+        println("0 9 18 27 36 45 54 ");
+        multiplicationTable(9,6);
+
+        println("");
+
+        //Test of sumUpTo method
+        int g = sumUpTo(4);
+        int h = sumUpTo(5);
+        int i = sumUpTo(100);
+        println("10: " + String.valueOf(g) + "\n15: " + String.valueOf(h) + "\n5050: " + String.valueOf(i));
     }
 }
